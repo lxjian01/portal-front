7f757deb5655186dc3ac78a24a414cfbd44925b5
lxjian01 <447689936@qq.com>
2021-05-19 10:53:01 +0800

    tt

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,34 +1,110 @@
-import axios from 'axios';
+import axios from 'axios'
+import { ElMessage } from 'element-plus'
+import store from '../store'
+import Cookies from 'js-cookie'
+export const TOKEN_KEY = 'Token ' // 空格必须
 
+let requestList = [];
+const saveTime = 1000;
+console.info(process.env)
+// create an axios instance
 const service = axios.create({
-    // process.env.NODE_ENV === 'development' 来判断是否开发环境
-    // easy-mock服务挂了，暂时不使用了
-    // baseURL: 'https://www.easy-mock.com/mock/592501a391470c0ac1fab128',
-    timeout: 5000
-});
+    baseURL: '/',
+    withCredentials: true,
+    timeout: 15 * 1000 // request timeout
+})
 
+// request interceptor
 service.interceptors.request.use(
     config => {
+
+        //定义取消请求函数
+        let cancel = undefined;
+        config.cancelToken = new axios.CancelToken(function executor(c) {
+            cancel = c;
+        });
+        //放置重复请求
+        let nowTime = new Date().getTime();
+        requestList = requestList.filter((item) => {
+            return (item.setTime + saveTime) > nowTime;
+        });
+        let sessionUrl = requestList.filter((item) => {
+            return item.url === config.url;
+        });
+        if (sessionUrl.length > 0) {
+            console.info(config.url + '请求重复 中断请求!');
+            cancel(config.url)
+            return;
+        }
+        // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
+        let token = Cookies.get("token");
+        if (token) {
+            config.headers['AUTHORIZATION'] = TOKEN_KEY + token
+        }
+        // cancel http cache
+        config.headers['Cache-Control'] = 'no-cache'
+        // set Content-type
+        if(config.headers['Content-type'] === undefined){
+            config.headers['Content-type'] = 'application/json;charset=utf-8'
+        }
         return config;
+
     },
     error => {
-        console.log(error);
-        return Promise.reject();
+        // do something with request error
+        console.error(error) // for debug
+        return Promise.reject(error)
     }
-);
+)
 
+// response interceptor
 service.interceptors.response.use(
     response => {
-        if (response.status === 200) {
-            return response.data;
-        } else {
-            Promise.reject();
+        const res = response.data
+        if (res.code === 2000) {
+            return res.data
         }
+        // if (res.code === 4000) {
+        //   return Promise.reject(res)
+        // }
+        ElMessage({
+            message: res.error || 'Error',
+            type: 'error',
+            duration: 3 * 1000
+        })
+        return Promise.reject(new Error(res || 'Error'))
     },
     error => {
-        console.log(error);
-        return Promise.reject();
+        const ErrorHandler = {
+            400: "请求错误",
+            401: "未授权，请登录",
+            403: "账号异常，请联系管理员",
+            404: `请求地址出错: ${error.response.config.url}`,
+            408: "请求超时",
+            500: "服务器内部错误",
+            501: "服务未实现",
+            502: "网关错误",
+            503: "服务不可用",
+            504: "网关超时",
+            505: "HTTP版本不受支持"
+        }
+        if (error && error.response) {
+            const { status, data } = error.response
+            if (status === 401) {
+                store.commit('user/LOGOUT')
+            }
+            error.message = status + ErrorHandler[status] || ''
+            if (data.error) {
+                error.message = data.error
+            }
+        }
+        ElMessage({
+            message: error.message,
+            type: 'error',
+            duration: 3 * 1000
+        })
+        return Promise.reject(error)
     }
-);
+)
 
-export default service;
+export default service
---
+++ vue.config.js
@@ -0,0 +1,89 @@
+'use strict'
+const path = require('path')
+const defaultSettings = require('./src/settings.js')
+
+function resolve (dir) {
+    return path.join(__dirname, dir)
+}
+
+const name = defaultSettings.title // page title
+
+// If your port is set to 80,
+// use administrator privileges to execute the command line.
+// For example, Mac: sudo npm run
+// You can change the port by the following method:
+// port = 4200 npm run dev OR npm run dev --port = 4200
+const port = process.env.port || process.env.npm_config_port || 8080 // dev port
+const TARGET = {
+    changeOrigin: true,
+}
+console.info(111)
+console.info(`request url is: ${process.env.VUE_APP_REQUEST_URL}`)
+// All configuration item explanations can be find in https://cli.vuejs.org/config/
+module.exports = {
+    /**
+     * You will need to set publicPath if you plan to deploy your site under a sub path,
+     * for example GitHub Pages. If you plan to deploy your site to https://foo.github.io/bar/,
+     * then publicPath should be set to "/bar/".
+     * In most cases please use '/' !!!
+     * Detail: https://cli.vuejs.org/config/#publicpath
+     */
+    publicPath: '/',
+    outputDir: 'dist',
+    assetsDir: 'static',
+    lintOnSave: process.env.NODE_ENV === 'development',
+    productionSourceMap: false,
+    devServer: {
+        port,
+        proxy: {
+            '^/api/portal/': {
+                target: `${process.env.VUE_APP_REQUEST_URL}`,
+                ...TARGET
+            },
+        },
+        overlay: {
+            warnings: false,
+            errors: true
+        }
+    },
+    configureWebpack: {
+        // provide the app's title in webpack's name field, so that
+        // it can be accessed in index.html to inject the correct title.
+        name,
+        resolve: {
+            alias: {
+                '@': resolve('src')
+            }
+        }
+    },
+    chainWebpack (config) {
+        // it can improve the speed of the first screen, it is recommended to turn on preload
+        // it can improve the speed of the first screen, it is recommended to turn on preload
+        config.plugin('preload').tap(() => [
+            {
+                rel: 'preload',
+                // to ignore runtime.js
+                // https://github.com/vuejs/vue-cli/blob/dev/packages/@vue/cli-service/lib/config/app.js#L171
+                fileBlacklist: [/\.map$/, /hot-update\.js$/, /runtime\..*\.js$/],
+                include: 'initial'
+            }
+        ])
+
+        // set svg-sprite-loader
+        config.module
+            .rule('svg')
+            .exclude.add(resolve('src/icons'))
+            .end()
+        config.module
+            .rule('icons')
+            .test(/\.svg$/)
+            .include.add(resolve('src/icons'))
+            .end()
+            .use('svg-sprite-loader')
+            .loader('svg-sprite-loader')
+            .options({
+                symbolId: 'icon-[name]'
+            })
+            .end()
+    }
+}
